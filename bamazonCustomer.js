6b57fbd0352b523a4594837480aec600ecbf1dc6
Ryan Weingart <rmweingart5@gmail.com>
2019-08-25 18:57:56 -0400

    added query to grab products from table from mysql and display in console

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -10,3 +10,21 @@ var connection = mysql.createConnection({
     database: "bamazon"
 })
 
+connection.connect(function(error) {
+    if (error) throw error;
+    log("Connection Successful!")
+
+    table();
+})
+
+function table() {
+    connection.query("SELECT * FROM products", function(err, res) {
+        for (var i = 0; i < res.length; i++) {
+            log("Item #" + res[i].item_id + "\n" + 
+                "Product: " + res[i].product_name + "\n" + 
+                "Department: " + res[i].department_name + "\n" + 
+                "Price: $" + res[i].price + "\n" + 
+                "Quantity Available: " + res[i].stock_quantity + "\n");
+        }
+    })
+}
