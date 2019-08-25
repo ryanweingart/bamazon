413616bd4f5817f9b0feac005040247cfec128b3
Ryan Weingart <rmweingart5@gmail.com>
2019-08-25 18:57:03 -0400

    added connection to mysql

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,6 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
 var log = console.log;
+
 var connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
@@ -9,21 +10,3 @@ var connection = mysql.createConnection({
     database: "bamazon"
 })
 
-connection.connect(function(error) {
-    if (error) throw error;
-    log("Connection Successful!")
-
-    table();
-})
-
-function table() {
-    connection.query("SELECT * FROM products", function(err, res) {
-        for (var i = 0; i < res.length; i++) {
-            log("Item #" + res[i].item_id + "\n" + 
-                "Product: " + res[i].product_name + "\n" + 
-                "Department: " + res[i].department_name + "\n" + 
-                "Price: $" + res[i].price + "\n" + 
-                "Quantity Available: " + res[i].stock_quantity + "\n");
-        }
-    })
-}
